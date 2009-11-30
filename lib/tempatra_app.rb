548943590527ecfaf549203211c334624780c807
Olivier Lauzon <olauzon@gmail.com>
2009-11-29 21:17:41 -0500

    Upgraded to ouput from 0.0.3

--- lib/tempatra_app.rb
+++ lib/tempatra_app.rb
@@ -1,17 +1,27 @@
 APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
 require 'rubygems'
+require 'compass'
 require 'sinatra'
 require 'haml'
-require 'sass'
 require 'rdiscount'
 
 class TempatraApp < Sinatra::Application
 
   set :root, APP_ROOT
 
+  configure do
+    Compass.configuration.parse(File.join(APP_ROOT, 'config.rb'))
+  end
+
   get '/' do
     haml :index
   end
+
+# Uncomment to compile your stylesheets without the compass utility
+#  get '/stylesheets/:name.css' do
+#    content_type 'text/css', :charset => 'utf-8'
+#    sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+#  end
 
 end
