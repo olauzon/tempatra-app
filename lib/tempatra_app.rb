5ec1ccb9e6d450e255e31289f6a5ee96519437f8
Olivier Lauzon <olauzon@gmail.com>
2010-08-31 20:36:26 -0400

    Updated to output from v0.2.0

--- lib/tempatra_app.rb
+++ lib/tempatra_app.rb
@@ -2,16 +2,17 @@ APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
 require 'rubygems'
 
-gem 'compass', '~> 0.10.2'
-require 'compass' # must be loaded before sinatra
+# Uncomment to compile your stylesheets without using 'compass compile' or 'compass watch'
+#gem 'compass', '~> 0.10.5'
+#require 'compass' # must be loaded before sinatra
 
 gem 'sinatra', '~> 1.0'
 require 'sinatra'
 
-gem 'haml', '~> 3.0.9'
+gem 'haml', '~> 3.0.18'
 require 'haml'    # must be loaded after sinatra
 
-gem 'rdiscount', '~> 1.6.3.2'
+gem 'rdiscount', '~> 1.6.5'
 require 'rdiscount'
 
 class TempatraApp < Sinatra::Application
@@ -19,18 +20,19 @@ class TempatraApp < Sinatra::Application
   set :root, APP_ROOT
   set :haml, { :format => :html5 } # default Haml format is :xhtml
 
-  configure do
-    Compass.add_project_configuration(File.join(APP_ROOT, 'compass.config'))
-  end
+# Uncomment to compile your stylesheets without using 'compass compile' or 'compass watch'
+#  configure do
+#    Compass.add_project_configuration(File.join(APP_ROOT, 'config/compass.rb'))
+#  end
 
   get '/' do
     haml :index
   end
 
-# Comment to compile your stylesheets with the compass utility
-  get '/stylesheets/:name.css' do
-    content_type 'text/css', :charset => 'utf-8'
-    sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
-  end
+# Uncomment to compile your stylesheets without using 'compass compile' or 'compass watch'
+#  get '/stylesheets/:name.css' do
+#    content_type 'text/css', :charset => 'utf-8'
+#    sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+#  end
 
 end
